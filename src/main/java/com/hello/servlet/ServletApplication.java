88b9103312a36fc3be01af1bbb9db8ee3eef0b2b
홍채민 <coals0329@cocen.com>
2022-05-04 15:51:07 +0900

    feat(spring-mvc): view resolver

--- src/main/java/com/hello/servlet/ServletApplication.java
+++ src/main/java/com/hello/servlet/ServletApplication.java
@@ -13,4 +13,11 @@ public class ServletApplication {
 		SpringApplication.run(ServletApplication.class, args);
 	}
 
+	// 스프링 부트가 해줌
+	// application.properties 에 등록한 spring.mvc.view.prefix , spring.mvc.view.suffix 설정 정보를 사용해서 등록
+	/* @Bean
+	ViewResolver internalResourceViewResolver() {
+		return new InternalResourceViewResolver("/WEB-INF/view/", ".jsp");
+	} */
+
 }
